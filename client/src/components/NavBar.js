9e344bfdaa856d2124ca8277ed60b61117756bc8
KDNagpl <Karandeepnagpal2@gmail.com>
2023-05-05 03:03:34 -0400

    finish the sign up page as well as some other updates to the nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,15 +8,48 @@ function NavBar({ currentPage, handlePageChange }) {
     <>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home" onClick={() => handlePageChange('Home')}
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Rapid Rentals</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home" onClick={() => handlePageChange('Home')}
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</Nav.Link>
-            <Nav.Link href="#catalog" onClick={() => handlePageChange('Catalog')}
-            className={currentPage === 'Catalog' ? 'nav-link active' : 'nav-link'}>Catalog</Nav.Link>
-            <Nav.Link href="#myorders" onClick={() => handlePageChange('MyRentals')}
-            className={currentPage === 'MyRentals' ? 'nav-link active' : 'nav-link'}>My Rentals</Nav.Link>
+          <Navbar.Brand
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+            Rapid Rentals
+          </Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              onClick={() => handlePageChange('Home')}
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#catalog"
+              onClick={() => handlePageChange('Catalog')}
+              className={
+                currentPage === 'Catalog' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Catalog
+            </Nav.Link>
+            <Nav.Link
+              href="#myorders"
+              onClick={() => handlePageChange('MyRentals')}
+              className={
+                currentPage === 'MyRentals' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              My Rentals
+            </Nav.Link>
+            <Nav.Link
+              href="#signup"
+              onClick={() => handlePageChange('Signup')}
+              className={
+                currentPage === 'Signup' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Sign up
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
